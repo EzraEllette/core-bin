1bdc86f7c82f4bb8786586e46f18fb1fb4fc793b
Owen Lenz <owen.lenz@gmail.com>
2021-03-10 21:21:46 -0500

    first commit to back_end branch

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const path = require("path");
 const { uid } = require("rand-token");
+const bodyParser = require('body-parser');
 
 const app = express();
-
 const bins = {};
 
+app.use(bodyParser.json());
 app.get("/", (req, res) => {
   res.json(bins);
 })
@@ -14,6 +15,7 @@ app.get("/getBin", (req, res) => {
   const token = uid(10);
   bins[token] = [];
   const binURL = `localhost:3000/r/${token}`;
+  console.log(req);
 
   res.send(binURL);
 })
@@ -28,6 +30,7 @@ app.all("/r/:token", (req, res) => {
 
   const { path, method, httpVersion, headers, body } = req;
   const requestObject = { path, method, httpVersion, headers, body };
+
 
   bins[token].push(requestObject);
   res.send(req.ip);
